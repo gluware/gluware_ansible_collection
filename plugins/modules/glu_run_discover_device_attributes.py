1785b00772af7a2b058e6e1b9d5008787e5e76b1
ogratwick <ogratwick@gluware.com>
2025-05-23 08:39:14 -0600

    updaed options documentation block

--- plugins/modules/glu_run_discover_device_attributes.py
+++ plugins/modules/glu_run_discover_device_attributes.py
@@ -27,7 +27,6 @@ DOCUMENTATION = '''
     author:
     - John Anderson (@gluware-inc)
     - Oleg Gratwick (@ogratwick-gluware)
-    options:
     extends_documentation_fragment:
     - gluware_inc.control.gluware_control
 
